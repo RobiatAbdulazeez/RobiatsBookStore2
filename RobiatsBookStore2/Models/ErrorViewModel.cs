de4084b8e294cba19a947231feed91117052d526
W0812675 <W0812675@WDL-A0336Z-38>
2023-11-15 17:56:23 -0500

    Add project files.

---
+++ RobiatsBookStore2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RobiatsBookStore2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
