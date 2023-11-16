826db90564cc646af119c713a5f4a23e2faed39a
Robiat Abdulazeez <W0812675@myscc.ca>
2023-11-15 19:20:29 -0500

    Created the new both areas that are given the slide and added the necessary veiws in it

--- RobiatsBookStore2/Startup.cs
+++ RobiatsBookStore2/Startup.cs
@@ -63,7 +63,7 @@ namespace RobiatsBookStore2
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
